ecc1502da1cbd3278f98aab8854e8a2f9dcc1490
Joseph Livengood <joeylivengood@gmail.com>
2016-10-01 13:09:29 +0000

    Set up app frame

--- server.js
+++ server.js
@@ -4,11 +4,17 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  //var query = req.query
+  res.send('TimeStamp Microservice');
+});
+
+app.get('/:id', function (req, res) {
+  var ui = req.params.id;
+  res.send('hello' + ui);
 });
 
 
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.listen(process.env.PORT, function () {
+  console.log('Example app listening on port ' + process.env.PORT +'!');
 });
